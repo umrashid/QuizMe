423cb1598b92eefe5714c835db4f39f7943cbd99
umrashid <umair.rashid@email.kpu.ca>
2019-07-08 15:04:43 -0700

    Added capital category questions

---
+++ Database/TableInserts/Questions.sql
@@ -0,0 +1,11 @@
+INSERT INTO Question (question, correct, option1, option2, option3, categoryID, difficultyID, questionTypeID, ImagePath)
+Values
+('What is the capital of Canada?', 'Ottawa', 'Toronto', 'Vancouver', 'Montreal', 1, 1, 1, null),
+('What is the capital of USA?', 'Washington DC', 'Chicago', 'Los Angeles', 'New York', 1, 1, 1, null),
+('What is the capital of UK?', 'London', 'Manchester', 'Liverpool', 'Birmingham', 1, 1, 1, null),
+('What is the capital of Egypt?', 'Cairo', 'Alexandria', 'Luxor', 'Giza', 1, 2, 1, null),
+('What is the capital of Australia?', 'Canberra', 'Sydney', 'Melbourne', 'Brisbane', 1, 2, 1, null),
+('What is the capital of Greece?', 'Athens', 'Mykonons', 'Santorini', 'Larissa', 1, 2, 1, null),
+('What is the capital of Iran?', 'Tehran', 'Mashhad', 'Isfahan', 'Karaj', 1, 3, 1, null),
+('What is the capital of Libya?', 'Tripoli', 'Benghazi', 'Misrata', 'Sirte', 1, 3, 1, null),
+('What is the capital of Romania?', 'Bucharest', 'Brasov', 'Lasi', 'Cluj-Napoca', 1, 3, 1, null);
